1a269c271db6b08d21098df5e0b5db27474cdcfd
Nick Golebiewski <nick.golebiewski@gmail.com>
2026-01-09 15:07:01 -0500

    Start on Tiled tilemap of NYC

---
+++ art/tiled/NEW_nyc_spritesheet-Recovered.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="NEW_nyc_spritesheet-Recovered" tilewidth="16" tileheight="16" tilecount="225" columns="15">
+ <image source="../aseprite_files/NEW_nyc_spritesheet-Recovered.png" trans="ff00ff" width="240" height="240"/>
+</tileset>
