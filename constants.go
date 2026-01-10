27a5813db134542fe0d1fab54104d77cb430eac4
Nick Golebiewski <nick.golebiewski@gmail.com>
2026-01-09 22:39:31 -0500

    🔍 demagnify

--- constants.go
+++ constants.go
@@ -2,6 +2,6 @@ package main
 
 // Watch out that constants don't get ridiculous!
 const (
-	screenWidth  = 640 // 320 //480 // was 320 + 160
-	screenHeight = 480 // 240 // 360 // was 240 + 120
+	screenWidth  = 480 //640 // 320 //480 // was 320 + 160
+	screenHeight = 360 //480 // 240 // 360 // was 240 + 120
 )
