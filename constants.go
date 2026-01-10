8a22327a7c1ee2dc23e4d48e35fdc2fbc05d9ec3
Nick Golebiewski <nick.golebiewski@gmail.com>
2026-01-09 22:33:28 -0500

    make screensize bigger, and 'shrink' the sprite maginification.

--- constants.go
+++ constants.go
@@ -2,6 +2,6 @@ package main
 
 // Watch out that constants don't get ridiculous!
 const (
-	screenWidth  = 320 //480 // was 320 + 160
-	screenHeight = 240 // 360 // was 240 + 120
+	screenWidth  = 640 // 320 //480 // was 320 + 160
+	screenHeight = 480 // 240 // 360 // was 240 + 120
 )
